ee66e9c5a6380b3a31427977dc94c9e3b181ea27
Sagar Das <65722439+Sagar60@users.noreply.github.com>
2022-07-06 15:20:38 +0530

    Update router.js

--- router.js
+++ router.js
@@ -33,10 +33,10 @@ const authenticateGoogle = () => {
   return auth;
 };
 
-const uploadToGoogleDrive = async (file, auth) => {
+const uploadToGoogleDrive = async (file, auth,GFolderId) => {
   const fileMetadata = {
     name: file.originalname,
-    parents: ["17HtmIgPahxaBxkyyNMUFiKExCMg4FJ1y"], // Change it according to your desired parent folder id
+    parents: [GFolderId], // Change it according to your desired parent folder id
   };
 
   const media = {
@@ -78,11 +78,45 @@ uploadRouter.post('/upload', multer.any(), async (req, res, next) => {
 	let response;
 	const files = req.files;
 	for (let f = 0; f < files.length; f += 1) {
-		response = await uploadToGoogleDrive(files[f], auth);
+		response = await uploadToGoogleDrive(files[f], auth,"17HtmIgPahxaBxkyyNMUFiKExCMg4FJ1y");	// folder-> Q&A
 		deleteFile(files[f].path)
 		// console.log(files[f]);
 		console.log('uploaded file number:' + String(f+1));
     }
+    // const response = await uploadToGoogleDrive(req.files[0], auth);
+    // deleteFile(req.files[0].path);
+    res.status(200).json({
+		"status": 200,
+		"id": response.data['id'],
+		"message": "File uploaded"
+		});
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+uploadRouter.post('/uploadUserImage', multer.any(), async (req, res, next) => {
+  try {
+	  //console.log(req.file,req.files[0]);
+    if (!req.files[0]) {
+		console.log("error coming on file retrieve");
+      res.status(404).json({
+		  "status": 404,
+		  "message": "File not found"
+	  });
+      return;
+    }
+	console.log("Files came successfully");
+	//console.log(req.file,req.files)
+	
+    const auth = authenticateGoogle();
+	let response;
+	const files = req.files;
+	response = await uploadToGoogleDrive(files[0], auth,"11DVgjb0BXmKJo6n4J9-WgdOhuY73XVmM");
+	deleteFile(files[0].path)
+		// console.log(files[f]);
+	console.log('uploaded user Image');
+	
     // const response = await uploadToGoogleDrive(req.files[0], auth);
     // deleteFile(req.files[0].path);
     res.status(200).json({
