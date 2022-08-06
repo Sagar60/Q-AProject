c9f8988adb66d30cc546ebf0152cc75b44de71a8
Sagar Das <65722439+Sagar60@users.noreply.github.com>
2022-08-06 16:52:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,25 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+// for parsing the body in POST request
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (_, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
 app.use(uploadRouter);
+
+app.on('error', error => {
+  console.error(error);
+});
+
+// running the server
 const port = process.env.PORT || 8080
 app.listen(port, () => {
   console.log('Form running on port '+ port);
